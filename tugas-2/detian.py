32741e460e33e3de6572498ec20ed242171e2ffd
Ahmad Faiz Muharram <faiz64muharram@gmail.com>
2026-02-03 21:13:36 +0700

    update fitur / perbaikan

--- tugas-2/detian.py
+++ tugas-2/detian.py
@@ -3,31 +3,36 @@ from monster import Monster
 class Iblis(Monster):
     def __init__(self, name, job, hp, max_hp, level):
         super().__init__(name, job, hp, max_hp, level, role="Raja Iblis")
-        self.rage = False  # status rage
+        self.rage = False  
 
-    # 🔥 cek rage mode
-    def check_rage(self):
-        if self.hp <= self.max_hp / 2 and not self.rage:
-            self.rage = True
-            print(f"\n== 👹 {self.name} Masuk Rage Mode ==\n")
+    def rage_mode(self):
+        if self.hp <= self.max_hp * 0.5 and not self.rage:
+           self.rage = True
+           print(f"=== 👹 {self.name} Memasuki Rage Mode ===")
+
 
     def critical(self, target):
-        dmg = 60
-        if self.rage:
-            dmg += 30  
-
+        dmg = 60 
         print(f"\n🔥 {self.name} menggunakan: 💥 Soul Mind Explosion!")
         print(f"🥊 {target.name} terkena {dmg} DMG dari {self.name}!")
 
         target.damage(dmg)
 
+    def damage(self, dmg):
+        super().damage(dmg)      
+        self.rage_mode()        
+
+
     def hool(self):
+        
         heal_amount = 20
         self.heal(heal_amount)
         print(f"\n🔥 {self.name} menggunakan: 🩸 Evil Soul Devouring!")
         print(f"💚 {self.name} menerima +{heal_amount} HP!")
 
     def domain(self, target1, target2, target3):
+        
+
         dmg = 40
         if self.rage:
             dmg += 20
